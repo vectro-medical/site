53b55840e80301c33eb4da298aac12fdd7d6afe9
kartikr <rkartik1992@gmail.com>
2022-02-20 20:01:56 +0530

    add email

--- src/app/contact/contact/contact.component.ts
+++ src/app/contact/contact/contact.component.ts
@@ -15,8 +15,8 @@ export class ContactComponent implements OnInit {
       state: 'Maharashtra',
       country: 'India',
       pincode: '431605',
-      mob: '+91 9975634785',
-      email: '',
+      mob: '9975634785',
+      email: 'vectromedicalsystem@gmail.com',
       gmapUrl:
         'https://www.google.co.in/maps/place/Kartik+Niwas/@19.186354,77.3069246,17z/data=!4m12!1m6!3m5!1s0x3bd1d69258caec97:0x11cb8da96aa0c026!2sKartik+Niwas!8m2!3d19.1858178!4d77.306507!3m4!1s0x3bd1d69258caec97:0x11cb8da96aa0c026!8m2!3d19.1858178!4d77.306507',
     },
